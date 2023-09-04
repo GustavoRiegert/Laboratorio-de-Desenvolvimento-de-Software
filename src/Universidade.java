41c2e088a8ec1713d34544f9ed06dad4f2ca0f85
NiloCan <titolianchen3341@gmail.com>
2023-09-04 20:22:15 -0300

    Delete src/Universidade.java

--- src/Universidade.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class Universidade {
-    private String nome, cnpj;
-    private List<Aluno> alunos;
-    private List<Professor> professores;
-    private List<Curso> cursos;
-    private Secretaria secretaria;
-}
